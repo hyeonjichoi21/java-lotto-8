ceb2bad78c09920f7c7c90144a752be6333b6d89
hyeonjichoi21 <ch5i_hj15@naver.com>
2025-11-03 17:40:07 +0900

    Feat(infra): RandomNumberPicker 파일 생성

--- src/main/java/lotto/infra/RandomNumberPicker.java
+++ src/main/java/lotto/infra/RandomNumberPicker.java
@@ -1,4 +1,14 @@
 package lotto.infra;
 
-public class RandomNumberPicker {
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RandomNumberPicker implements NumberPicker {
+    @Override
+    public List<Integer> pickSix() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6).stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
 }
